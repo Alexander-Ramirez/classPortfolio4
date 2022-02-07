44ef8aba966fab1fec7e617a2892c1ad521ea72b
Alex.Ramirez <aleramirez201@gmail.com>
2022-02-07 12:10:34 -0600

    added base spots for code and currently adding my code from my provious home made portfolios

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,14 @@
+const AboutPage = () => {
+    return (
+        <section id="About-Me" class="main-info">
+            <img src="https://via.placeholder.com/150" alt="placeholder" class="float-left" />
+            <h2>About Me</h2>
+            <p>
+                Hello my name is Alex. I am a beginning programmer learing Full Stack with emphasis on Front End Development, like HTML, CSS, and JavaScript to name a few.
+            </p>
+        </section>
+
+    );
+};
+
+export default AboutPage;
